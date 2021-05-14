7dbeb6377758ef21fc5928be068c642ec2eea0c4
wongpratan <wongpratan@hotmail.com>
2021-05-14 15:33:19 +0700

    *Reformat the file

--- utils/active_timer.js
+++ utils/active_timer.js
@@ -3,51 +3,59 @@ const cron = require("node-cron");
 var TIMER_POOLS = {};
 
 module.exports = {
+   start: (req, element) => {
+      if (element == null || !element.isEnabled) return;
 
+      let tenantID = req.tenantID();
+      if (TIMER_POOLS[tenantID] == null) {
+         TIMER_POOLS[tenantID] = {};
+      }
 
-    start: (req, element) => {
-        if (element == null || !element.isEnabled) return;
+      // Stop
+      if (TIMER_POOLS[tenantID][element.id] != null) {
+         this.stop(req, element.id);
+      }
 
-        let tenantID = req.tenantID();
-        if (TIMER_POOLS[tenantID] == null) {
-            TIMER_POOLS[tenantID] = {};
-        }
+      let cronExpression = element.getCronExpression();
+      req.log(`::: START CRON job [${cronExpression}] - ${element.id}`);
 
-        // Stop
-        if (TIMER_POOLS[tenantID][element.id] != null) {
-            this.stop(req, element.id);
-        }
+      TIMER_POOLS[tenantID][element.id] = cron.schedule(cronExpression, () => {
+         // start the processs task
+         req.log(
+            `::: TRIGGER CRON job [${cronExpression}] - ${element.id} - ${element.triggerKey}`
+         );
 
-        let cronExpression = element.getCronExpression();
-        req.log(`::: START CRON job [${cronExpression}] - ${element.id}`);
+         req.serviceRequest(
+            "process_manager.trigger",
+            {
+               key: element.triggerKey,
+               data: {}
+            },
+            () => {
+               req.log("Timer started");
+            }
+         );
+      });
+   },
 
-        TIMER_POOLS[tenantID][element.id] = cron.schedule(cronExpression, () => {
-            // start the processs task
-            req.log(
-                `::: TRIGGER CRON job [${cronExpression}] - ${element.id} - ${element.triggerKey}`
-            );
+   /**
+    * @function stop
+    *
+    * @param {uuid} elementId - the id of ABProcessTriggerTimer
+    */
+   stop: (req, elementId) => {
+      let tenantID = req.tenantID();
 
-            req.serviceRequest("process_manager.trigger", {
-                key: element.triggerKey,
-                data: {}
-            }, () => {
-                req.log("Timer started");
-            });
-        });
-    },
+      if (
+         elementId == null ||
+         TIMER_POOLS[tenantID] == null ||
+         TIMER_POOLS[tenantID][elementId] == null
+      )
+         return;
 
-    /**
-* @function stop
-*
-* @param {uuid} elementId - the id of ABProcessTriggerTimer
-*/
-    stop: (req, elementId) => {
-        let tenantID = req.tenantID();
+      req.log(`::: Stop CRON job - ${elementId}`);
+      TIMER_POOLS[tenantID][elementId].stop();
+      delete TIMER_POOLS[tenantID][elementId];
+   }
+};
 
-        if (elementId == null || TIMER_POOLS[tenantID] == null || TIMER_POOLS[tenantID][elementId] == null) return;
-
-        req.log(`::: Stop CRON job - ${elementId}`);
-        TIMER_POOLS[tenantID][elementId].stop();
-        delete TIMER_POOLS[tenantID][elementId];
-    }
-}

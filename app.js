09f48a5ca727235a503b97f2ce6ff2a0aebcc3ca
Nate <7259@pm.me>
2023-10-31 12:28:29 +0700

    add sentry tracking

--- app.js
+++ app.js
@@ -3,7 +3,17 @@
 // (AppBuilder) a micro service to manage our process tasks
 //
 const AB = require("@digiserve/ab-utils");
-
+const { version } = require("./package");
+// Use sentry by default, but can override with env.TELEMETRY_PROVIDER
+if (AB.defaults.env("TELEMETRY_PROVIDER", "sentry") == "sentry") {
+   AB.telemetry.init("sentry", {
+      dsn: AB.defaults.env(
+         "SENTRY_DSN",
+         "https://ff0af8f37828480d7791c2e58c0682a3@o144358.ingest.sentry.io/4506143282298880"
+   ),
+      release: version,
+   });
+}
 var controller = AB.controller("process_manager");
 
 // controller.afterStartup((req, cb) => {
